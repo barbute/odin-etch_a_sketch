3a6247a3a0d73ec75791c62435faabc1a6cfb980
barbute <jackhdemeterio@gmail.com>
2025-05-09 14:16:10 -0500

    Working grid displayed properly
    NOTE Was not able to style the container in JS an the CSS files, so had to move all of the container styles to be inside of the JS file

--- src/app.js
+++ src/app.js
@@ -4,3 +4,39 @@
  * This file is part of sojourner-spud and is licensed under the MIT License.
  * See the LICENSE file in the root of the project for more information.
  */
+
+const container = document.querySelector(".container");
+
+// Every pixel will have a light border that will be visible so the user can
+// see the pixels - NOTE This MUST be in multiples of 0.5
+const pixelBorderSizePX = 0.5;
+
+// How many pixels the canvas grid should be
+const gridSizeX = 16;
+const gridSizeY = 16;
+const gridFullSize = 
+  (gridSizeX + pixelBorderSizePX) * (gridSizeX + pixelBorderSizePX);
+
+// How many screen pixels each canvas pixel should be
+const pixelSizeXPX = 16;
+const pixelSizeYPX = 16;
+
+// Adding styles to constrain the number of grid squares
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.flexBasis = `${gridFullSize}px`;
+
+// Generate grid
+for (let x = 0; x < gridSizeX; x++) {
+  for (let y = 0; y < gridSizeY; y++) {
+    const pixel = document.createElement("div");
+    // Assign pixel class so we can apply a style to all pixels on the canvas.
+    pixel.setAttribute("class", "pixel");
+
+    pixel.style.flexBasis = `${pixelSizeXPX}px`;
+    pixel.style.height = `${pixelSizeYPX}px`;
+    pixel.style.border = `${pixelBorderSizePX}px #d4d4d4 dashed`;
+
+    container.appendChild(pixel);
+  }
+}
